40ce4d6d42b66b3c35404aa14ede85a79a8be945
Garrett Dancik <garrett.dancik@gmail.com>
2020-06-15 15:12:13 -0400

    Update

--- R/graphics.R
+++ R/graphics.R
@@ -34,8 +34,8 @@ display_plot <- function(g1, tooltip = c("x", "y"), formatDate = TRUE) {
 # %>% partial_bundle()
 }
 
-saveUnemployment <- function(g, file, dir = getwd()) {
+saveUnemployment <- function(g, file, dir = getwd(), tooltip = c("x", "y", "fill") {
     file <- paste0(dir,'/',file)
-    htmlwidgets::saveWidget((g%>%display_plot(tooltip = c("x","y","fill")) %>%
+    htmlwidgets::saveWidget((g%>%display_plot(tooltip = tooltip) %>%
                           partial_bundle()), file = file)
 }
